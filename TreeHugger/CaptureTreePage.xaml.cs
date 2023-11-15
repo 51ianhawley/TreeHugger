a3d48c848e835b1f9da487c9898663f873ba6aad
Ian Hawley <51ianhawley@gmail.com>
2023-11-15 12:54:20 -0600

    Added camera UI.

--- TreeHugger/CaptureTreePage.xaml.cs
+++ TreeHugger/CaptureTreePage.xaml.cs
@@ -1,4 +1,4 @@
-using Camera.MAUI;
+
 
 namespace TreeHugger;
 
@@ -11,8 +11,27 @@ public partial class CaptureTreePage : ContentPage
 
 	private async void btnTakePhoto_Clicked(object sender, EventArgs e)
 	{
+        if (MediaPicker.Default.IsCaptureSupported)
+        {
+            FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
 
-	}
+            if (photo != null)
+            {
+                // save the file into local storage
+                
+                string localFilePath = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+
+                using Stream sourceStream = await photo.OpenReadAsync();
+                using FileStream localFileStream = File.OpenWrite(localFilePath);
+                await sourceStream.CopyToAsync(localFileStream);
+                Console.WriteLine("A picture has been successfully takne.");
+                Console.WriteLine(localFileStream.ToString());
+                
+                imgTree.Source = ImageSource.FromFile(localFilePath);
+                //await sourceStream.CopyToAsync(localFileStream);
+            }
+        }
+    }
 
 
 }
